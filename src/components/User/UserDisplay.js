7a72586f022f97fbe8a5b46c940504d087180b9a
Otim Brian <otims.brian@gmail.com>
2024-01-24 00:37:33 +0300

    Finished working on User display

--- src/components/User/UserDisplay.js
+++ src/components/User/UserDisplay.js
@@ -1,25 +1,27 @@
 import avatar from "../../static/images/avatar/avatar.jpeg"
+import { FaEnvelope, FaBuildingColumns } from "react-icons/fa6";
 
 const UserDisplay = () => {
     return (
-        <div className="user-detail">
+        <div className="user-display">
             <img src={avatar} alt="Avatar" />
 
-            <div className="user-detail-container">
-                <p><strong>Name:</strong> Otim Brian</p>
-                <p><strong>Username:</strong> Otim</p>
+            <div className="user-display-container">
+                <div id="personal">
+                    <article><strong>Name:</strong> Otim Brian</article>
+                    <article><strong>Username:</strong> Otim</article>
+                    <article><FaEnvelope /> <strong>Email:</strong> test-email@gmail.com</article>
+                </div>
+                <div id="technical">
+                    <article><strong><FaBuildingColumns /> Departments:</strong></article>
+                    {/* <br /> */}
 
-                {/* <br /> */}
-
-                <p><strong>Email:</strong> test-email@gmail.com</p>
-                
-                    <strong>Departments:</strong>
-                    <ul>
+                    <ul id="department">
                         <li>Department</li>
                         <li>Department</li>
                         <li>Department</li>
                     </ul>
-               
+                </div>
             </div>
         </div>
     )
