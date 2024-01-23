2cfe3d23f6eb0c25802f93bbb203f8b3efb6c598
Otim Brian <otims.brian@gmail.com>
2024-01-23 03:56:56 +0300

    worked on card

---
+++ src/components/User/Card.js
@@ -0,0 +1,37 @@
+
+
+const UserCard = () => {
+    return (
+        <div className="card-wrapper">
+            <div className="card">
+                <div className="card-head"></div>
+                <div className="card-body">
+                    <h2 className="title">Ant Collector</h2>
+                    <p className="desc">Morgan has collected ants since they were six years old and now has many dozen ants but none in their pants.</p>
+                    <div className="actions">
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                    </div>
+                </div>
+            </div>
+            <div className="card hover">
+                <div className="card-head">
+                    <h2>Morgan Sweeney</h2>
+                </div>
+            </div>
+            <div className="card-body">
+                <h2 className="title">Ant Collector</h2>
+                <p className="desc">Morgan has collected ants since they were six years old and now has many dozen ants but none in their pants.</p>
+                <div className="actions">
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default UserCard
