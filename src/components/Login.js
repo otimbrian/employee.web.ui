b6556ae9cc454adc653a0a5e9281c2c759e0e117
Otim Brian <otims.brian@gmail.com>
2024-01-28 02:52:22 +0300

    Finished working on the login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,67 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
-import loginService from "../services/loginService"
+import { Link, useNavigate } from "react-router-dom"
+// import loginService from "../services/loginService"
 import Notification from './Notification.js'
+// import { unwrapResult } from "@reduxjs/toolkit"
 import { useDispatch } from 'react-redux'
 import { disableNotification, postNotification } from "../reducers/notificationReducer.js"
+import { userLogin } from "../reducers/userReducer.js"
+import setUserToken, { employeeLocalStorage } from "../services/shared.js"
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
+    // Function to handle Notifiction assignment.
+    const post = (message, status) => {
+
+        dispatch(postNotification({
+            message: message,
+            status: "error"
+        }))
+        setTimeout(
+            () => {
+                dispatch(disableNotification())
+            }, 5000)
+
+    }
+
+    // Login Handler.
     const handleLogin = async (event) => {
         event.preventDefault()
-        console.log("Loging in .....", { email, password })
+        console.log("Loging in ------->", { email, password })
 
         try {
-            const respone = await loginService.genericUserLogin({ email, password })
-            console.log(respone);
+            // User login with email and password.
+            const result = await dispatch(userLogin({ email, password })).unwrap()
+            console.log("login result ----->", result);
+
+            // Add the token to the local storage.
+            employeeLocalStorage.addToLocalStorage(employeeLocalStorage.TOKEN, result)
+
+            // Set the token value for future requests.
+            // in the shared folder.
+            setUserToken(result.token)
+
+            // Navigate to the home page
+            navigate("/")
+
+        // If any error occurs in the login
+            // Or the credentials are rejected?
+            // 
+            // Notify the client.
         } catch (exception) {
-            console.log("Error in logging in: --->", exception.message)
-            dispatch(postNotification({
-                message: exception.message,
-                status: "error"
-            }))
-            setTimeout(
-                () => {
-                    dispatch(disableNotification())
-                    console.log("=====Into another ====");
-                }, 5000)
+            
+            console.log("Error in logging in: --->", exception)
+
+            // When the email or password or both are invalid.
+            if(exception.name === "TypeError") {
+                post("network error", "error")
+            }else{
+                post(exception.message, "error")
+            }
         }
 
     }
@@ -40,7 +74,7 @@ const Login = () => {
                     <label htmlFor="email">
                         <h3>Email</h3>
                         <input type="email" onChange={(event) => setEmail(event.target.value)}
-                            placeholder="Enter Email" required name="email" value={email} id="email"/>
+                            placeholder="Enter Email" required name="email" value={email} id="email" />
                     </label>
 
                     <label htmlFor="password">
