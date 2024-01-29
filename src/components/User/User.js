d3620f57fc01ad5d3e114fbf3f01d25d3e7ec0a3
Otim Brian <otims.brian@gmail.com>
2024-01-30 01:28:21 +0300

    Finished user view

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,26 +1,29 @@
-
-import ButtonLink from "../ButtonLink"
-import UserDisplay from "./UserDisplay"
+import ButtonLink from '../ButtonLink'
+import UserDisplay from './UserDisplay'
 import { useSelector } from 'react-redux'
-import { FaUser } from "react-icons/fa6";
+import { FaUser } from 'react-icons/fa6'
+import { employeeLocalStorage } from '../../services/shared'
 
 const User = () => {
     const user = useSelector(state => state.user.userObject)
-    console.log("user in user display ---->", user);
 
     const logout = () => {
-        console.log("loggin out.");
+        // console.log('loggin out.')
+        employeeLocalStorage.removeFromLocalStorage(employeeLocalStorage.NAME)
     }
     return (
         <>
-            <div className="employee-content" id="logout">
-                <button id="user-logout" onClick={logout}><FaUser/>Logout</button>
+            <div className='employee-content' id='logout'>
+                <button id='user-logout' onClick={logout}>
+                    <FaUser />
+                    Logout
+                </button>
             </div>
             <br />
-            <div className="employee-content">
+            <div className='employee-content'>
                 <UserDisplay user={user} />
             </div>
-            <ButtonLink link={"/employee/edit"} value={"Edit User"} />
+            <ButtonLink link={'/employee/edit'} value={'Edit User'} />
         </>
     )
 }
