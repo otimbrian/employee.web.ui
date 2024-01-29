c070248214009eb0f4134a55bcaac859634fd6c3
Otim Brian <otims.brian@gmail.com>
2024-01-30 01:22:06 +0300

    Finished user display

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,13 +1,26 @@
+
 import ButtonLink from "../ButtonLink"
 import UserDisplay from "./UserDisplay"
+import { useSelector } from 'react-redux'
+import { FaUser } from "react-icons/fa6";
 
 const User = () => {
+    const user = useSelector(state => state.user.userObject)
+    console.log("user in user display ---->", user);
+
+    const logout = () => {
+        console.log("loggin out.");
+    }
     return (
         <>
-            <div className="employee-content">
-                <UserDisplay />
+            <div className="employee-content" id="logout">
+                <button id="user-logout" onClick={logout}><FaUser/>Logout</button>
             </div>
-            <ButtonLink />
+            <br />
+            <div className="employee-content">
+                <UserDisplay user={user} />
+            </div>
+            <ButtonLink link={"/employee/edit"} value={"Edit User"} />
         </>
     )
 }
