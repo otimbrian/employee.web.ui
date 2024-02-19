3ad5ae06c308146441324c7e249e0b4cbf6f9d34
Otim Brian <otims.brian@gmail.com>
2024-02-19 13:47:06 +0300

    Worked on deprtment view

--- src/components/Department/DepartmentView.js
+++ src/components/Department/DepartmentView.js
@@ -1,9 +1,54 @@
+import {
+    FaBuildingColumns,
+    FaUserGroup,
+} from 'react-icons/fa6'
 import ButtonLink from '../ButtonLink'
+import NavigateBack from '../NavigateBack'
+
+
+
+const EmployeeList = ({ employees }) => {
+    return (
+        <ul >
+            {employees.map(employee => (
+                <li key={employee.id}>{employee.name}</li>
+            ))}
+        </ul>
+    )
+}
 
 const DepartmentView = ({department}) => {
     return (
         <>
-            <div>Department View</div>
+        <div className = "employee-content">
+        <NavigateBack />
+        </div>
+        <br />
+        <br />
+            <div className="employee-content">
+               
+            <div className='department-view'>
+            <article>
+                <FaBuildingColumns />
+            </article>
+            <div className='depart-view-container'>
+                <h4>
+                    <i>{department.name}</i>
+                </h4>
+                <p>
+                    <FaUserGroup /> <strong>{department.employees.length}</strong> <br />
+                    Employees
+                </p>
+
+                <div>
+                <p>
+                    <strong>EMPLOYEES</strong>
+                </p>
+                <EmployeeList employees={department.employees} />
+                </div>
+            </div>
+            </div>
+            </div>
             <ButtonLink link={`/department/edit/${department.id}`} value={'Edit department'} />
         </>
     )
